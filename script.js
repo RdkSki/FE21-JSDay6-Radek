2f0f27a717f58559afb022c9bf3bbc66c73fadb1
RdkSki <90190905+RdkSki@users.noreply.github.com>
2021-09-30 11:17:49 +0200

    basic finished

--- script.js
+++ script.js
@@ -2,8 +2,26 @@
 var sandwiches = `{ "sandwich": "hamburger", "calories": "260" }`;
 var fries = `{ "fries_size": "Large French Fries", "calories": "570" }`;
 
+// let object1 = {
+//     "sandwich": "hamburger",
+//     "calories": "260"
+// };
 
-/*My favorite sandwich is a Hamburger which has approximately 260 calories, along with it I enjoy 
-eating Large French Fries which have about 570 calories.*/
+let object1 = JSON.parse(sandwiches);
+
+document.getElementById('result').innerHTML = 
+`My favorite sandwich is a ${object1.sandwich} which has approximately ${object1.calories} calories, `;
+
+// let object2 = {
+//     "fries_size": "Large French Fries",
+//     "calories": "570"
+// };
+
+let object2 = JSON.parse(fries);
+
+document.getElementById('result').innerHTML += 
+`along with it I enjoy eating ${object2.fries_size} which have about ${object2.calories} calories.`;
+
+
 
 
