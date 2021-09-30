222f0569fa45a0b5fd0584f801ad824ee5559385
RdkSki <90190905+RdkSki@users.noreply.github.com>
2021-09-30 15:15:57 +0200

    locale storage added

--- script2.js
+++ script2.js
@@ -4,5 +4,14 @@ console.log(mydata);
 for (let val of mydata){
     document.getElementById("results").innerHTML += `<p>title: ${val.title}</p>
     <p>author: ${val.author}</p>
-    <img src="${val.img}"><hr>`;
+    <img src="${val.img}" width="120"><hr>`;
+    if (val.read == "true"){
+        document.getElementById("results").style.background.green;
+    }else{
+        document.getElementById("results").style.background.red;
+    }
 }
+
+localStorage.setItem("books", JSON.stringify(mydata));
+
+localStorage.getItem(JSON.parse(mydata));
