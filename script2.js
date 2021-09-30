2995532a7829bbf23c450f5e43d4b594d22eb2e8
RdkSki <90190905+RdkSki@users.noreply.github.com>
2021-09-30 14:41:26 +0200

    last step

--- script2.js
+++ script2.js
@@ -1,2 +1,8 @@
 let mydata = JSON.parse(books);
 console.log(mydata);
+
+for (let val of mydata){
+    document.getElementById("results").innerHTML += `<p>title: ${val.title}</p>
+    <p>author: ${val.author}</p>
+    <img src="${val.img}"><hr>`;
+}
