d4fdd2fc19bdf958e51dd421e988a53399e2047e
RdkSki <90190905+RdkSki@users.noreply.github.com>
2021-09-30 15:24:38 +0200

    adv final

--- script2.js
+++ script2.js
@@ -2,16 +2,40 @@ let mydata = JSON.parse(books);
 console.log(mydata);
 
 for (let val of mydata){
-    document.getElementById("results").innerHTML += `<p>title: ${val.title}</p>
-    <p>author: ${val.author}</p>
-    <img src="${val.img}" width="120"><hr>`;
+    
     if (val.read == "true"){
-        document.getElementById("results").style.background.green;
+        document.getElementById("results").innerHTML += `<div style="background: green"><p>title: ${val.title}</p>
+        <p>author: ${val.author}</p>
+        <img src="${val.img}" width="120"><hr></div>`;
     }else{
-        document.getElementById("results").style.background.red;
+        document.getElementById("results").innerHTML += `<div style="background: red"><p>title: ${val.title}</p>
+        <p>author: ${val.author}</p>
+        <img src="${val.img}" width="120"><hr></div>`;
     }
 }
 
-localStorage.setItem("books", JSON.stringify(mydata));
+// localStorage.setItem("books", JSON.stringify(mydata));
 
-localStorage.getItem(JSON.parse(mydata));
+// localStorage.getItem(JSON.parse(mydata));
+
+
+// for (let x of playersAfterParse) {
+//     console.log(Object.keys(x)[3])
+
+
+//     if (x.gym == true) {
+//         document.getElementById("result").innerHTML += `<div class="col-4" style="background: green">
+//         <p>${x.name}</p>
+//         <p>${x.age}</p>
+//         <img src="${x.img}" width="300">
+//     </div>`;
+//     } else {
+//         document.getElementById("result").innerHTML += `<div class="col-4" style="background: red">
+//         <p>${x.name}</p>
+//         <p>${x.age}</p>
+//         <img src="${x.img}" width="300">
+//     </div>`;
+
+//     }
+
+// }
